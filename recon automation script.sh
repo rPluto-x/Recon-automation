a15ce430343109d0529b3fd0ff328b4c363b131d
rPluto <ojojamesadedeji@gmail.com>
2026-06-10 10:34:26 -0700

    Fix label lines and nlookup

--- recon automation script.sh
+++ recon automation script.sh
@@ -9,16 +9,16 @@ fi
 mkdir -p "recon_results"
 
 ping_4=$(ping -c 4 $1)
-dns_lookup=$(nslookup 4 $1)
+dns_lookup=$(nslookup $1)
 user_print=$(whois $1)
 
-cat "=== PING ===" >> recon_results/$1.txt
+echo "=== PING ===" >> recon_results/$1.txt
 echo "$ping_4" >> recon_results/$1.txt
 
-cat "=== DNS LOOKUP ==='" >> recon_results/$1.txt
+echo "=== DNS LOOKUP ==='" >> recon_results/$1.txt
 echo "$dns_lookup" >> recon_results/$1.txt
 
-cat "=== WHOIS ===" >> recon_results/$1.txt
+echo "=== WHOIS ===" >> recon_results/$1.txt
 echo "$user_print" >> recon_results/$1.txt
 
 echo "Recon complete. Results saved to recon_results/$1.txt"
